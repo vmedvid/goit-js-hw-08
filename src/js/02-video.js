499b1f13e99ecb9708c1ef8365a9c9e5dd0535a6
vmedvid <vmedvid22@gmail.com>
2023-03-09 23:09:43 +0200

    зробив завд.2-3 в ДЗ#8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+const save = (key, value) => {
+  try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
+    console.error('Set state error: ', error.message);
+  }
+};
+
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? 0 : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+player.on(
+  'timeupdate',
+  throttle(function (evt) {
+    save(LOCALSTORAGE_KEY, evt.seconds);
+  }, 1000)
+);
+
+player.setCurrentTime(load(LOCALSTORAGE_KEY));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,62 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const inputEl = formEl.elements.email;
+const textareaEl = formEl.elements.message;
+
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+inputEl.value = load(LOCALSTORAGE_KEY).email || '';
+textareaEl.value = load(LOCALSTORAGE_KEY).message || '';
+
+const inputedData = {
+  email: inputEl.value,
+  message: textareaEl.value,
+};
+
+function save(key, value) {
+  try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
+    console.error('Set state error: ', error.message);
+  }
+}
+
+function load(key) {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? '' : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+}
+
+function remove(key) {
+  localStorage.removeItem(key);
+}
+
+formEl.addEventListener('input', throttle(saveMessage, 500));
+
+function saveMessage(evt) {
+  inputedData.email = inputEl.value;
+  inputedData.message = textareaEl.value;
+
+  save(LOCALSTORAGE_KEY, inputedData);
+}
+
+formEl.addEventListener('submit', submitForm);
+
+function submitForm(evt) {
+  evt.preventDefault();
+
+  if (load(LOCALSTORAGE_KEY)) {
+    console.log('inputed+submit data:', load(LOCALSTORAGE_KEY));
+  } else {
+    console.log('inputed+submit data:', { email: '', message: '' });
+  }
+
+  remove(LOCALSTORAGE_KEY);
+
+  evt.currentTarget.reset();
+}
